7fc8e73acf8512f2360030aac7d6833a18ad4cf3
niceluckyday <908800197@qq.com>
2024-02-19 11:48:00 +0800

    Update build_lede.sh

--- build_lede.sh
+++ build_lede.sh
@@ -3,11 +3,18 @@
 # Navigate into LEDE directory and execute subsequent commands
 cd lede && \
 
+# Update LEDE repository
+git pull && \
+
+# Update and install feeds
+./scripts/feeds update -a && \
+./scripts/feeds install -a && \
+
 # Generate .config file non-interactively
 make defconfig && \
 
 # Copy your custom .config file
-wget https://raw.githubusercontent.com/niceluckyday/bianyi/main/.config -O .config && \
+# wget https://raw.githubusercontent.com/niceluckyday/bianyi/main/.config -O .config && \
 
 # Start compilation
 make download -j$(nproc) && \

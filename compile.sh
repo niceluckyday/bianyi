2a88a34883e1886fa4911c1cea4eb2904f1f5571
niceluckyday <908800197@qq.com>
2024-02-19 11:10:30 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-# Clone the LEDE repository
-git clone https://github.com/coolsnowwolf/lede
-cd lede
+# Clone the LEDE repository, navigate into it, and execute subsequent commands
+git clone https://github.com/coolsnowwolf/lede && \
+cd lede && \
 
 # Add additional package source
-sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default && \
 
 # Update and install feeds
-./scripts/feeds update -a
-./scripts/feeds install -a
+./scripts/feeds update -a && \
+./scripts/feeds install -a && \
 
 # Modify default settings
-sed -i "s#DISTRIB_REVISION='.*'#DISTRIB_REVISION='$(TZ=UTC-8 date "+%Y.%m.%d")'#" package/lean/default-settings/files/zzz-default-settings
-sed -i "s#DISTRIB_DESCRIPTION='.*'#DISTRIB_DESCRIPTION='Lucky '#" package/lean/default-settings/files/zzz-default-settings
-sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
+sed -i "s#DISTRIB_REVISION='.*'#DISTRIB_REVISION='$(TZ=UTC-8 date "+%Y.%m.%d")'#" package/lean/default-settings/files/zzz-default-settings && \
+sed -i "s#DISTRIB_DESCRIPTION='.*'#DISTRIB_DESCRIPTION='Lucky '#" package/lean/default-settings/files/zzz-default-settings && \
+sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate && \
 
 # Change Luci theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile && \
 
 # Change kernel version
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile && \
 
 # Generate .config file non-interactively
-make defconfig
+make defconfig && \
 
 # Copy your custom .config file
-wget https://raw.githubusercontent.com/niceluckyday/bianyi/main/.config -O .config
+wget https://raw.githubusercontent.com/niceluckyday/bianyi/main/.config -O .config && \
 
 # Start compilation
-make download -j$(nproc)
+make download -j$(nproc) && \
 make V=s -j$(nproc)

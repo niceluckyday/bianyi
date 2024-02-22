374451fb41948375245b3d7c5d739167824b8480
niceluckyday <908800197@qq.com>
2024-02-23 07:42:28 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 
 # Clone the LEDE repository, navigate into it, and execute subsequent commands
+
 git clone https://github.com/coolsnowwolf/lede && \
 cd lede && \
 
 # Add additional package source
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default && \
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default && \
+
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default" && \
 
 # Update and install feeds
+
 ./scripts/feeds update -a && \
 ./scripts/feeds install -a && \
 
 # Modify default settings
+
 sed -i "s#DISTRIB_REVISION='.*'#DISTRIB_REVISION='\$(TZ=UTC-8 date \"+%Y.%m.%d\")'#" package/lean/default-settings/files/zzz-default-settings && \
 sed -i "s#DISTRIB_DESCRIPTION='.*'#DISTRIB_DESCRIPTION='Lucky '#" package/lean/default-settings/files/zzz-default-settings && \
 sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate && \
 
 # Change Luci theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argone/' feeds/luci/collections/luci/Makefile && \
+
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile && \
+
+# Add Luci theme compilation package
+
+cd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon && \
+
+# Add Luci theme settings package
+
+rm -rf luci-app-argon-config # if it exists
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config && \
 
 # Change kernel version
+
 # sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile && \
 
 # Generate .config file non-interactively
+
 make defconfig && \
-
-# Copy your custom .config file
-wget https://raw.githubusercontent.com/niceluckyday/bianyi/main/.config -O .config && \
-
-# Start compilation
-make download -j$(nproc) && \
-make V=s -j$(nproc)

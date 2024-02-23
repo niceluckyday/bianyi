ca5b117ababe707f639e7875aca988534ed866f0
niceluckyday <908800197@qq.com>
2024-02-23 08:47:14 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -13,14 +13,6 @@ echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main"
 ./scripts/feeds update -a && \
 ./scripts/feeds install -a && \
 
-# Modify default settings
-sed -i "s#DISTRIB_REVISION='.*'#DISTRIB_REVISION='\$(TZ=UTC-8 date \"+%Y.%m.%d\")'#" package/lean/default-settings/files/zzz-default-settings && \
-sed -i "s#DISTRIB_DESCRIPTION='.*'#DISTRIB_DESCRIPTION='Lucky '#" package/lean/default-settings/files/zzz-default-settings && \
-sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate && \
-
-# Change Luci theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile && \
-
 # Add Luci theme compilation package
 cd package/lean
 rm -rf luci-theme-argon
@@ -29,6 +21,20 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-
 # Add Luci theme settings package
 rm -rf luci-app-argon-config # Remove if it exists
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config && \
+
+# 返回根目录
+cd
+
+# 重新进入lede目录
+cd lede &&
+
+# Modify default settings
+sed -i "s#DISTRIB_REVISION='.*'#DISTRIB_REVISION='\$(TZ=UTC-8 date \"+%Y.%m.%d\")'#" package/lean/default-settings/files/zzz-default-settings && \
+sed -i "s#DISTRIB_DESCRIPTION='.*'#DISTRIB_DESCRIPTION='Lucky '#" package/lean/default-settings/files/zzz-default-settings && \
+sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate && \
+
+# Change Luci theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile && \
 
 # Change kernel version
 # sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile && \
